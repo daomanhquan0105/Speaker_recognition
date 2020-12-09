f150af63db0aaf92a5a38b29bef6ef050bf419cb
chickenZeo <hhthanh.mta@gmail.com>
2020-12-09 13:58:37 +0700

    init commit1

--- SR/test.py
+++ SR/test.py
@@ -9,7 +9,7 @@ import pandas as pd
 from recordAudio import record_audio, record_one_file
 
 
-def test_list_file(quantity_file, record=False):
+def test_list_file(quantity_file=2, record=False):
     if record == True:
         record_audio(quantity_file, c.RECORD_SECONDS, c.FILE_NAME_TEST, c.TEST_SET, True)
 
@@ -20,7 +20,8 @@ def test_list_file(quantity_file, record=False):
 
     models = [pickle.load(open(file_name, 'rb')) for file_name in gmm_files]
     speakers = [file_name.split('\\')[-1].split(".gmm")[0] for file_name in gmm_files]
-    file_paths = open(c.FILE_NAME_TEST, 'r')
+    # file_paths = open(c.FILE_NAME_TEST, 'r')
+    file_paths = os.listdir(c.TEST_SET)
     cols = ['file_name', 'scores', 'speaker']
     df_result = pd.DataFrame(columns=cols)
     # test_list_file(file_paths, models, speakers)
@@ -40,7 +41,7 @@ def test_list_file(quantity_file, record=False):
         new_data = [path, scores, speaker]
         new_df = pd.DataFrame([new_data], columns=cols)
         df_result = pd.concat([df_result, new_df], ignore_index=True)
-    df_result.to_csv('res/result.csv')
+    df_result.to_csv('res/result1.csv')
     print("Sumit successfully!result to save dir res/result.csv")
 
 
@@ -70,7 +71,6 @@ def test_one_file(file_name):
         log_likelihood[i] = scores.sum()
 
     winner = np.argmax(log_likelihood)
-
     speaker = speakers[winner].split('/')[-1]
     time.sleep(1)
     print(f"{file_name} la {speaker} nói")
@@ -81,7 +81,7 @@ if __name__ == '__main__':
         choice = int(
             input("\n 1.Test list files \n 2.Test 1 file \n "))
         if (choice == 1):
-            test_list_file(12,True)
+            test_list_file()
         elif (choice == 2):
             test_one_file("file-record-test.wav")
         if (choice > 2):
